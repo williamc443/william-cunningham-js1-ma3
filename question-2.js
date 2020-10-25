e42b7924be7b182cc41ed02dd8f02920d27a0ef3
williamc443 <57712080+williamc443@users.noreply.github.com>
2020-10-25 22:56:37 +0100

    Update question-2.js

--- question-2.js
+++ question-2.js
@@ -1,1 +1,37 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
+const container = document.querySelector(".container");
+
+async function Gamefacts() {
+    try {
+        const response = await fetch(url);
+        const results = await response.json();
+        const games = results.results;
+
+        container.innerHTML = "";
+
+        for(let i = 0; i < games.length; i++) {
+
+                if(i === 8) {
+                    break;
+                }
+
+         container.innerHTML +=      
+            `<div>
+                <h2>${games[i].name}</h2>
+                <p>Rating: ${games[i].rating}</p>
+                <p>Number of Tags: ${games[i].tags.length}</p>
+            </div>`;
+        }
+
+    } catch (error) {
+        console.log("error occured", error);
+        container.innerHTML = "Not able to load, error occured!";
+    }
+      finally {
+        console.log("finaly");
+        container.innerHTML = "The request was confirmed!";
+    }
+  }
+  
+  Gamefacts();
